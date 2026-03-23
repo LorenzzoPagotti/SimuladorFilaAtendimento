7f9cfce3291d9d037b3c1380f2ac4d2a082feb42
Kauã V. <nokknee10@gmail.com>
2026-03-23 18:18:41 -0300

    final

--- main.c
+++ main.c
@@ -1,34 +1,35 @@
 #include "simulador.h" // isso ja da include em td que precisa
 
-int main ()
+int main()
 {
     int opcao;
+    int tempo;
 
     do
     {
-        printf ("=== SIMULADOR DE FILA DE ATENDIMENTO ===");
-        printf ("\n1. Rodar Simulação");
-        printf ("\n2. Sair");
+        printf("=== SIMULADOR DE FILA DE ATENDIMENTO ===");
+        printf("\n1. Rodar Simulação");
+        printf("\n2. Sair");
 
-        scanf ("%d", &opcao);
+        scanf("%d", &opcao);
 
         switch (opcao)
         {
-            case 1:
-            // ***
+        case 1:
+            printf("Quantos minutos a simulacao deve durar? ");
+            scanf("%d", &tempo);
+            executarSimulacao(tempo);
             break;
 
-            case 2:
+        case 2:
             printf("Saindo do simulador...\n");
             break;
 
-            default:
+        default:
             printf("Opção inválida!\n");
         }
 
-
     } while (opcao != 2);
-    
+
     return 0;
-    
 }
