d93ef9c2e717864fbc938d2f9e171ffe0cfeaef4
Kauã V. <nokknee10@gmail.com>
2026-03-23 18:23:36 -0300

    Update simulador.h

--- simulador.h
+++ simulador.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void executarSimulacao(int tempo_total_minutos);
 void gerar_numero_rand(void);
 void imprimir_estatisticas(int tempo_total, int n_clientes_atendidos)
 {
